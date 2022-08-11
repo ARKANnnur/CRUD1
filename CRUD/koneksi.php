5d87cd3c5786fbfd0bf68aedef0ffd541ce948c0
ARKANnnur <105828063+ARKANnnur@users.noreply.github.com>
2022-08-12 00:14:36 +0700

    Delete CRUD directory

--- CRUD/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $server = "localhost";
-    $user = "root";
-    $pass = "";
-    $db = "db_ukk2022";
-
-    $conn = mysqli_connect($sever, $user, $pass, $db) or die ("Galal Terhubung");
-?>
